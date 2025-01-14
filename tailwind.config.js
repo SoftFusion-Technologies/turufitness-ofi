42606c1a8473e6d20f305f329d80f55da45b2aaa
Benjamin <goosta19802@gmail.com>
2025-01-14 18:09:52 -0300

    Se configura talwind y se agrega la paleta de colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1E90FF',
+        secondary: '#D16BA5',
+        dark: '#1A1A1A',
+        white: '#FFFFFF',
+        purple: '#6C63FF',
+        lightGray: '#F5F5F5'
+      }
+    }
   },
-  plugins: [],
-}
-
+  plugins: []
+};
