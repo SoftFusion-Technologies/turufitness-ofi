0a2698527144a73becfb593bbebd1b138b275287
Benjamin <goosta19802@gmail.com>
2025-01-16 05:54:30 -0300

    Se mejora el chosee us

--- src/Components/ChooseUs.jsx
+++ src/Components/ChooseUs.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import CountUp from 'react-countup'; // Importar la librería
 import '../Styles/ChoseUs.css';
 import ImgChoose from '../Images/choose-img.png';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const ChooseUs = () => {
+  useEffect(() => {
+    AOS.init({
+      // duration: 1000, // Duración de la animación en milisegundos
+      easing: 'ease-in-out', // Tipo de animación
+      once: true // Si la animación se repite al hacer scroll
+    });
+  }, []);
+
+  const [startCount, setStartCount] = useState(false);
+  const counterRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setStartCount(true);
+        }
+      },
+      { threshold: 0.5 } // Se activa cuando el 50% del elemento es visible
+    );
+
+    if (counterRef.current) {
+      observer.observe(counterRef.current);
+    }
+
+    return () => {
+      if (counterRef.current) {
+        observer.unobserve(counterRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <section className="choose section" id="choose">
-      <div className="choose_overflow">
+    <section className="choose section" id="choose" data-aos="fade-up">
+      <div className="choose_overflow" ref={counterRef}>
         <div className="choose_container container grid">
           <div className="choose_content">
             <div className="section_data">
@@ -30,12 +64,9 @@ const ChooseUs = () => {
               <div className="choose_group">
                 <h3 className="choose_number">
                   +
-                  <CountUp
-                    start={0}
-                    end={300}
-                    duration={2}
-                    separator=","
-                  />{' '}
+                  {startCount && (
+                    <CountUp start={0} end={300} duration={3} separator="," />
+                  )}{' '}
                   {/* Animación para el número */}
                 </h3>
                 <p className="choose_subtitle">Miembros Activos</p>
@@ -43,7 +74,9 @@ const ChooseUs = () => {
               <div className="choose_group">
                 <h3 className="choose_number">
                   +
-                  <CountUp start={0} end={5} duration={2} separator="," />{' '}
+                  {startCount && (
+                    <CountUp start={0} end={5} duration={3} separator="," />
+                  )}{' '}
                   {/* Animación para el número */}
                 </h3>
                 <p className="choose_subtitle">Entrenadores Profesionales</p>
@@ -55,8 +88,11 @@ const ChooseUs = () => {
                 </p>
               </div>
               <div className="choose_group">
-                <h3 className="choose_number">+
-                  <CountUp start={0} end={30} duration={2} separator="," />{' '}
+                <h3 className="choose_number">
+                  +
+                  {startCount && (
+                    <CountUp start={0} end={30} duration={3} separator="," />
+                  )}{' '}
                   {/* Animación para el número */}
                 </h3>
                 <p className="choose_subtitle">Equipos de Última Tecnología</p>
@@ -66,9 +102,9 @@ const ChooseUs = () => {
 
           <div className="choose_images">
             <img src={ImgChoose} alt="gym image" className="choose_img" />
-            {/* <div className="choose_triangle choose_triangle-1"></div> */}
-            {/* <div className="choose_triangle choose_triangle-2"></div> */}
-            {/* <div className="choose_triangle choose_triangle-3"></div> */}
+            <div className="choose_triangle choose_triangle-1"></div>
+            <div className="choose_triangle choose_triangle-2"></div>
+            <div className="choose_triangle choose_triangle-3"></div>
           </div>
         </div>
       </div>
